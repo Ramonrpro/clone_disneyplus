a85a12a21c2a3491d46512b6b6e785ca8c4c12f8
Ramonrpro <122824092+Ramonrpro@users.noreply.github.com>
2023-10-05 10:41:17 -0300

    criando o hero e importando a font avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({ outputStyle: 'compressed'}))
     .pipe(gulp.dest('./dist/css'));
 }
+function img() {
+    return gulp.src('./src/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/img'));
+}
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, img);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
